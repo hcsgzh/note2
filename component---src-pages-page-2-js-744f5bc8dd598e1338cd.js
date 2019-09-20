c532686086d8a387a23aac0aa36197ec2f5f5de4
hcsgzh <hcsgzh@gmail.com>
2019-09-20 18:04:43 +1200

    Updates

---
+++ component---src-pages-page-2-js-744f5bc8dd598e1338cd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{218:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(59),c=a(214),r=a(215);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-744f5bc8dd598e1338cd.js.map
---
+++ styles-cc4fdc21137ba6bb201c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{224:function(n,w,o){}}]);
+//# sourceMappingURL=styles-cc4fdc21137ba6bb201c.js.map
